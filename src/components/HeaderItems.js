b3e0af80feee0a0b4847e915c78134238f584c68
Jonathan Tey <jonathantey@airasia.com>
2021-08-24 13:45:43 +0800

    update mini.css

--- src/components/HeaderItems.js
+++ src/components/HeaderItems.js
@@ -1,23 +1,30 @@
-import React from 'react'
-import PropTypes from 'prop-types'
+import React from "react";
+import PropTypes from "prop-types";
 
-import {Button} from 'reactstrap'
+import { Button } from "reactstrap";
 
+const handleShow = (name) => {
+  document
+    .getElementById(name)
+    .scrollIntoView({ block: "start", behavior: "smooth" });
+};
 
-const handleShow = name => {
-  document.getElementById(name).scrollIntoView({block: 'start', behavior: 'smooth'})
-}
-
-const HeaderItems = ({title, link}) => (
-  <Button key={title} color="link" className="button" onClick={handleShow.bind(this, link)}>
+const HeaderItems = ({ title, link }) => (
+  <Button
+    style={{ background: "#12171a", color: "#f5f5f5" }}
+    key={title}
+    color="link"
+    className="button"
+    onClick={handleShow.bind(this, link)}
+  >
     {title}
   </Button>
-)
+);
 
 HeaderItems.propTypes = {
   title: PropTypes.string.isRequired,
   link: PropTypes.string.isRequired,
-  handleShow: PropTypes.func.isRequired
-}
+  handleShow: PropTypes.func.isRequired,
+};
 
-export default HeaderItems
+export default HeaderItems;
--- src/containers/Header.js
+++ src/containers/Header.js
@@ -1,13 +1,16 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
-import HeaderItems from '../components/HeaderItems'
+import HeaderItems from "../components/HeaderItems";
 
-const Header = ({sections}) => {
+const Header = ({ sections }) => {
   return (
-    <header className="sticky">
-      {sections.map(section => HeaderItems({...section}))}
+    <header
+      style={{ backgroundColor: "#12171a", border: 0 }}
+      className="sticky"
+    >
+      {sections.map((section) => HeaderItems({ ...section }))}
     </header>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
